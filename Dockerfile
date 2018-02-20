254433ae018c4c4e7e7fe1fc8fca438463cf2d7a
Andrey Kuzmin <prepodam@mail.ru>
2018-02-20 22:21:39 +0300

    Postgresql 10.2, stolon 0.9, etcd version.
    Updated postgresql and stolon to latest versions.
    Consul support removed, etcd support added.
    Container with 3 services in one splitted to 3 containers. Now you need to start 3 containers: with sentinel, keeper and proxy (docker way).

--- Dockerfile
+++ Dockerfile
@@ -1,47 +1,49 @@
-FROM postgres:9.6
+FROM postgres:10.2
 
-ENV STOLON_VERSION=0.5.0
-ENV STOLON_SHA256=4766fe7227ecbcdc032a83586cabe52c6e01e0a9a3d8a095f40e9da913be0d7f
+ENV \
+  STOLON_VERSION=0.9.0 \
+  STOLON_SHA256=e31ece535a8a6343fc66a5612b62389da87ba266f705ac6a8d32c746a4d3cdfa \
+  \
+  STOLON_SERVICE=\
+  ETCD_HTTP_ADDR= \
+  PG_REPL_USERNAME= \
+  PG_REPL_PASSWORD= \
+  PG_SU_PASSWORD= \
+  EXT_HOSTNAME= \
+  STOLON_CLUSTER_NAME= \
+  STOLON_IP= \
+  \
+  SET_CONTAINER_TIMEZONE=true \
+  CONTAINER_TIMEZONE=Europe/Moscow \
+  \
+  LANG=ru_RU.UTF-8
 
 RUN \
   apt-get update \
   && apt-get install --no-install-recommends --no-install-suggests -y \
-  curl ca-certificates \
-
+    ca-certificates \
+    curl \
+  \
   && cd /usr/local/bin \
   && curl -L https://github.com/sorintlab/stolon/releases/download/v${STOLON_VERSION}/stolon-v${STOLON_VERSION}-linux-amd64.tar.gz -o stolon-v${STOLON_VERSION}-linux-amd64.tar.gz \
   && echo -n "$STOLON_SHA256  stolon-v${STOLON_VERSION}-linux-amd64.tar.gz" | sha256sum -c - \
   && tar -zxvf stolon-v${STOLON_VERSION}-linux-amd64.tar.gz \
-
-  && cp ./stolon-v${STOLON_VERSION}-linux-amd64/stolon-keeper ./ \
-  && cp ./stolon-v${STOLON_VERSION}-linux-amd64/stolon-proxy ./ \
-  && cp ./stolon-v${STOLON_VERSION}-linux-amd64/stolon-sentinel ./ \
-  && cp ./stolon-v${STOLON_VERSION}-linux-amd64/stolonctl ./ \
-
+  \
+  && cp ./stolon-v${STOLON_VERSION}-linux-amd64/bin/stolon-keeper ./ \
+  && cp ./stolon-v${STOLON_VERSION}-linux-amd64/bin/stolon-proxy ./ \
+  && cp ./stolon-v${STOLON_VERSION}-linux-amd64/bin/stolon-sentinel ./ \
+  && cp ./stolon-v${STOLON_VERSION}-linux-amd64/bin/stolonctl ./ \
+  \
   && rm -rf ./stolon-v${STOLON_VERSION}-linux-amd64 \
   && rm stolon-v${STOLON_VERSION}-linux-amd64.tar.gz \
-
+  \
   && localedef -i ru_RU -c -f UTF-8 -A /usr/share/locale/locale.alias ru_RU.UTF-8 \
-
-  && apt-get remove -y curl ca-certificates \
-  && apt-get autoremove -y \
+  \
+  && apt-get purge -y --auto-remove \
+    ca-certificates \
+    curl \
   && rm -rf /var/lib/apt/lists/*
-
-ENV CONSUL_HTTP_ADDR=
-ENV CONSUL_TOKEN=
-
-ENV PG_REPL_USERNAME=
-ENV PG_REPL_PASSWORD=
-ENV PG_SU_PASSWORD=
-ENV EXT_HOSTNAME=
-ENV STOLON_CLUSTER_NAME=
-ENV STOLON_IP=
-
-ENV SET_CONTAINER_TIMEZONE=true
-ENV CONTAINER_TIMEZONE=Europe/Moscow
-
-ENV LANG=ru_RU.UTF-8
 
 COPY stolon-entrypoint.sh /usr/local/bin/stolon-entrypoint.sh
 
-ENTRYPOINT [ "/usr/local/bin/stolon-entrypoint.sh" ]
+ENTRYPOINT ["/usr/local/bin/stolon-entrypoint.sh"]
--- stolon-entrypoint.sh
+++ stolon-entrypoint.sh
@@ -9,67 +9,42 @@ else
   echo "Container timezone not modified"
 fi
 
-if [ -z "$STOLON_IP" ]; then
-  STOLON_IP=$( hostname -i | awk '{print $1}' )
-  echo "IP detected $STOLON_IP"
-fi
-
 id=$( echo $EXT_HOSTNAME | awk -F '.' '{ print $1 }' | sed 's/[^a-z0-9\_]/_/g' )
 echo $id
 
-/usr/local/bin/stolon-sentinel --cluster-name $STOLON_CLUSTER_NAME \
-  --store-backend consul \
-  --store-endpoints $CONSUL_HTTP_ADDR &
-SS_PID=$!
+chown postgres:postgres /var/lib/postgresql/data
 
-/usr/local/bin/stolon-proxy --cluster-name $STOLON_CLUSTER_NAME \
-  --store-backend consul \
-  --store-endpoints $CONSUL_HTTP_ADDR \
-  --listen-address 0.0.0.0 \
-  --port 5432 &
-SP_PID=$!
+if [ "$STOLON_SERVICE" = "sentinel" ]; then
+  gosu postgres /usr/local/bin/stolon-sentinel \
+    --cluster-name $STOLON_CLUSTER_NAME \
+    --store-backend etcdv3 \
+    --store-endpoints $ETCD_HTTP_ADDR &
+  child=$!
+elif [ "$STOLON_SERVICE" = "proxy" ]; then
+  gosu postgres /usr/local/bin/stolon-proxy \
+    --cluster-name $STOLON_CLUSTER_NAME \
+    --store-backend etcdv3 \
+    --store-endpoints $ETCD_HTTP_ADDR \
+    --listen-address 0.0.0.0 \
+    --port 5432 &
+  child=$!
+elif [ "$STOLON_SERVICE" = "keeper" ]; then
+  gosu postgres /usr/local/bin/stolon-keeper  \
+    --cluster-name $STOLON_CLUSTER_NAME \
+    --store-backend etcdv3 \
+    --store-endpoints $ETCD_HTTP_ADDR \
+    --data-dir /var/lib/postgresql/data \
+    --pg-bin-path /usr/lib/postgresql/10/bin/ \
+    --pg-repl-password $PG_REPL_PASSWORD \
+    --pg-repl-username $PG_REPL_USERNAME \
+    --pg-su-password $PG_SU_PASSWORD \
+    --uid $id \
+    --pg-listen-address $STOLON_IP \
+    --pg-port 7432 &
+    child=$!
+fi
 
-chown postgres /var/lib/postgresql/data/
-
-gosu postgres /usr/local/bin/stolon-keeper  --cluster-name $STOLON_CLUSTER_NAME \
-  --data-dir /var/lib/postgresql/data/ \
-  --pg-bin-path /usr/lib/postgresql/9.6/bin/ \
-  --pg-repl-password $PG_REPL_PASSWORD \
-  --pg-repl-username $PG_REPL_USERNAME \
-  --pg-su-password $PG_SU_PASSWORD \
-  --store-backend consul \
-  --store-endpoints $CONSUL_HTTP_ADDR \
-  --uid $id \
-  --pg-listen-address $STOLON_IP \
-  --pg-port 7432 &
-SK_PID=$!
-
-echo "Pids sentinel $SS_PID proxy $SP_PID keeper $SK_PID"
-
-trap "kill $SS_PID ; kill $SP_PID ; kill $SK_PID" TERM INT
-
-while true; do
-  kill -0 "$SS_PID"
-  if [ "$?" = "0" ]; then
-    sleep 5
-  else
-    echo "Exited sentinel"
-    exit
-  fi
-
-  kill -0 "$SP_PID"
-  if [ "$?" = "0" ]; then
-    sleep 5
-  else
-    echo "Exited proxy"
-    exit
-  fi
-
-  kill -0 "$SK_PID"
-  if [ "$?" = "0" ]; then
-    sleep 5
-  else
-    echo "Exited keeper"
-    exit
-  fi
-done
+trap "kill $child" INT TERM
+wait "$child"
+trap - INT TERM
+wait "$child"
